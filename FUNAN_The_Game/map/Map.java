d44c62a0b0040ed33f177842e06e90476451f289
Chris <christoph.berndl@hotmail.de>
2018-04-15 11:35:46 +0200

    Map and Square class added

---
+++ FUNAN_The_Game/map/Map.java
@@ -0,0 +1,23 @@
+import javafx.stage.Window;
+
+public class Map {
+
+	int height;
+	int width;
+	private String [] backgroundImagePaths;
+	private String [] foregroundImagePaths;
+	private Square [][] square;
+	
+	public Map (int height, int width, String[] backgroundImagePaths, String[] foregroundImagePaths, Square[][] square){
+		
+		this.height = height;
+		this.width = width;
+		this.backgroundImagePaths = backgroundImagePaths;
+		this.foregroundImagePaths = foregroundImagePaths;
+		this.square = square;
+		
+		 
+		
+	}
+	
+}
